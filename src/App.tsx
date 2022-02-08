9429c862825effec41f1f3781da92b99622bab03
Rafał Sobota <ravsobota@gmail.com>
2022-02-08 21:52:12 +0100

    center

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import FunctionalUniphaser from "./uniphaser/FunctionalUniphaser";
 
 function App() {
   return (
-    <div className="w-full h-full text-center bg-white dark:bg-slate-900">
+    <div className="flex flex-col w-full h-full text-center place-content-center">
       <FunctionalUniphaser />
     </div>
   );
