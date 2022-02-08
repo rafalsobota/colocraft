7bb5717e9278feb8129ee1338c257e897d13548c
Rafał Sobota <ravsobota@gmail.com>
2022-02-08 18:43:44 +0100

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import FunctionalUniphaser from "./uniphaser/FunctionalUniphaser";
 
 function App() {
   return (
-    <div className="text-center">
+    <div className="w-full h-full text-center bg-white dark:bg-slate-900">
       <FunctionalUniphaser />
     </div>
   );
