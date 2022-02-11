eab97b97a18823ff6d42fc908183170d44da445d
Rafał Sobota <ravsobota@gmail.com>
2022-02-11 17:28:14 +0100

    Fix warnings in crypto

--- src/game/crypto.ts
+++ src/game/crypto.ts
@@ -3,7 +3,7 @@
 function xmur3(str: string): () => any {
   for (var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
     h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
-    h = h << 13 | h >>> 19;
+    h = (h << 13) | (h >>> 19);
   } return function () {
     h = Math.imul(h ^ (h >>> 16), 2246822507);
     h = Math.imul(h ^ (h >>> 13), 3266489909);
@@ -14,9 +14,9 @@ function xmur3(str: string): () => any {
 function mulberry32(a: number): () => number {
   return function () {
     var t = a += 0x6D2B79F5;
-    t = Math.imul(t ^ t >>> 15, t | 1);
-    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-    return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    t = Math.imul(t ^ (t >>> 15), t | 1);
+    t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+    return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
   }
 }
 
